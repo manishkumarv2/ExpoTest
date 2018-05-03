a619ef25ec525a44fc111f8c96ed002192c0f18f
manish.k <manish.k@v2solutions.com>
2018-05-03 16:25:09 +0530

    remove axios related code from files

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import axios from 'axios';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
 
-componentWillMount(){
-    // Make a request for a user with a given ID
-    axios.get('https://demo-profile.herokuapp.com/profile')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
 
   render() {
     return (
