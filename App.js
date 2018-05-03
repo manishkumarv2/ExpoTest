3c7e997e1203b343eafb52e6036cb26f7fe10d5e
manish.k <manish.k@v2solutions.com>
2018-05-03 16:28:22 +0530

    changed the background color

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#a8ff30',
+    backgroundColor: '#fff130',
     alignItems: 'center',
     justifyContent: 'center',
   },
