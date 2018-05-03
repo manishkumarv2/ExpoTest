9a585c6d3e2f69b963f9a0c6238b3c73d810e2af
manish.k <manish.k@v2solutions.com>
2018-05-03 15:15:44 +0530

    Change the background colo

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f',
+    backgroundColor: '#fff000',
     alignItems: 'center',
     justifyContent: 'center',
   },
